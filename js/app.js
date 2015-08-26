5da4bc0c450c2f98796646fdfdaec7799c693075
Arnold Krumins <arnold.krumins@gmail.com>
2015-08-26 21:52:30 +0200

    messing about

--- js/app.js
+++ js/app.js
@@ -6,19 +6,46 @@ var app = (function(){
     var map1 = Immutable.Map({a:1, b:2, c:3});
     var map2 = map1.set('b', 50);
 
+    var familyOne = [
+        {name:'Arnold', age:48, nationality:'British', sex:'m'},
+        {name:'Anna', age:1, nationality:'Swedish', sex:'f'},
+        {name:'Alice', age:3, nationality:'Swedish', sex:'f'},
+        {name:'Amie', age:34, nationality:'Swedish', sex:'f'}
+        ];
+
+    var familyTwo = [
+        {name:'Arnold', age:48, nationality:'British', sex:'m'},
+        {name:'Anna', age:1, nationality:'Swedish', sex:'f'},
+        {name:'Alice', age:3, nationality:'Swedish', sex:'f'},
+        {name:'Zoe', age:40, nationality:'English', sex:'f'}
+    ];
+
+
+    var map3 = Immutable.List(familyOne);
+    var map4 = Immutable.List(familyTwo);
+
     test = function(){
 
         console.log(map1.toJS());
         console.log(map2.toJS());
-    }
+    };
 
 
+    merge = function(){
+
+        var result =  map3.toSet().union(map4.toSet()).toList().toJS();
+        console.log(map3.toJS());
+        console.log(map4.toJS());
+        console.log(result);
+
+    };
 
     return{
 
-        test:test
+        test:test,
+        merge:merge
 
-    }
+    };
 
 
 }());
